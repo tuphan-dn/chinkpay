02536b78c9588d2df8d61c2aa4c77179483b48dd
tuphan-dn <tuphan@descartes.network>
2023-11-25 10:39:17 +0700

    chore: setup rpx, key, abi

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox-viem'
+import 'hardhat-abi-exporter'
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.19',
+  abiExporter: {
+    path: './abi',
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    only: [':Lock$'],
+  },
+  networks: {
+    sepolia: {
+      url: process.env.RPC || '',
+      accounts: [process.env.PRIVKEY || ''],
+    },
+  },
+}
 
-export default config;
+export default config
